1c1cfe6df01e46a8e90044d4118aa138e24864db
divyasinghshah <divyasinghshah@gmail.com>
2022-08-05 13:34:02 +0530

    map

--- index.js
+++ index.js
@@ -6,16 +6,25 @@ const port = 8000;
 
 const path=require('path');
 const db=require('./config/mongoose');
-
+const Hotel=require('./models/Hotel');
 // app.use(bodyParser.urlencoded())
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+
 
 app.use(express.urlencoded());
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/tourist',require('./routes/tourist'));
 app.use('/api/hotel',require('./routes/hotel'));
 app.use('/api/booking',require('./routes/booking'));
+app.use('/api/map',async function(req,res){
+    let hotels=await Hotel.find();
+    
+    return res.render('map',{
+        hotels:hotels
+    });
+
+});
 
 app.get('/',function(req,res){
     return res.render('home');
